1054b4447ab3c2bd978afca90d1b6f8c5be187be
Surya Bhupatiraju <surya95@gmail.com>
2015-01-29 03:16:54 -0500

    added title and body post textareas

--- master/helloworld.cpp
+++ master/helloworld.cpp
@@ -5,6 +5,9 @@
 #include <Wt/WPushButton>
 #include <Wt/WText>
 #include <Wt/WCssDecorationStyle>
+#include <Wt/WText>
+#include <Wt/WTextArea>
+#include <Wt/WDateTime>
 
 class HelloApplication : public Wt::WApplication
 {
@@ -13,7 +16,10 @@ public:
 
 private:
     Wt::WLineEdit *blogEdit_;
+	Wt::WText *title_;
     Wt::WText *post_;
+	Wt::WTextArea *bodyEdit_;
+	Wt::WText *bodyPost_;
 
     void greet();
 	void enter();
@@ -26,10 +32,24 @@ HelloApplication::HelloApplication(const Wt::WEnvironment& env)
     Wt::WText *title = new Wt::WText("<h1>A Witty Blog: The Daily Xu</h1>");
     root()->addWidget(title);
 
-    root()->addWidget(new Wt::WText("Blog Post: "));
-    blogEdit_ = new Wt::WLineEdit(root());
-    Wt::WPushButton *button = new Wt::WPushButton("Enter post.", root());
+    root()->addWidget(new Wt::WText("Blog Title: "));
     root()->addWidget(new Wt::WBreak());
+    blogEdit_ = new Wt::WLineEdit(root());
+	blogEdit_->addStyleClass("post");
+
+	root()->addWidget(new Wt::WBreak());
+	bodyEdit_ = new Wt::WTextArea(root());
+	bodyEdit_->setColumns(80);
+	bodyEdit_->setRows(5);
+	bodyEdit_->setText("Enter body of post!");
+	bodyEdit_->addStyleClass("post");
+
+	root()->addWidget(new Wt::WBreak());
+    Wt::WPushButton *button = new Wt::WPushButton("Enter post.", root());
+	button->addStyleClass("post");
+	
+    root()->addWidget(new Wt::WBreak());
+
     post_ = new Wt::WText(root());
     button->clicked().connect(this, &HelloApplication::enter);
   	blogEdit_->enterPressed().connect(this, &HelloApplication::enter);
@@ -43,8 +63,16 @@ void HelloApplication::greet()
 void HelloApplication::enter()
 { 
     root()->addWidget(new Wt::WBreak());
-    post_ = new Wt::WText(root());
-    post_->setText(blogEdit_->text()); 
+    title_ = new Wt::WText(root());
+    title_->setText(blogEdit_->text()); 
+	root()->addWidget(new Wt::WBreak());
+	post_ = new Wt::WText(root());
+	post_->setText(bodyEdit_->text());
+	root()->addWidget(new Wt::WBreak());
+	root()->addWidget(new Wt::WBreak());
+
+	blogEdit_->setText("New title");
+	bodyEdit_->setText("New body");
 }	
 
 Wt::WApplication *createApplication(const Wt::WEnvironment& env)
